e0ed996b7c50a8e90fcb067b0f85031c83de1f9a
Duncan Sterken <duncan.sterken123@gmail.com>
2019-01-15 14:41:21 +0100

    Try out rider

--- 16-nov-2017-Vault/Form1.cs
+++ 16-nov-2017-Vault/Form1.cs
@@ -11,144 +11,145 @@ using System.Windows.Forms;
 namespace _16_nov_2017_Vault {
     public partial class Form1 : Form {
         //This holds the monitor
-        SerialMonitor serialMonitor = null;
+        private readonly SerialMonitor _serialMonitor;
         //What is the code
-        int[] password = new int[]{2,1,3};
+        private readonly int[] _password = {2,1,3};
         //What did the user enter
-        List<int> enteredCode = new List<int>();
+        private readonly List<int> _enteredCode = new List<int>();
         //Dummy var
-        String light = "green";
+        private string _light = "green";
         //All lights off
-        Boolean lightsOn = false;
-        //Timer liftime
-        int liftime = 0;
+        private bool _lightsOn;
+        //Timer lifetime
+        private int _lifetime;
 
         public Form1() {
             InitializeComponent();
             //Regiser the monitor
-            serialMonitor = new SerialMonitor(this.RTBLogDSTE);
+            _serialMonitor = new SerialMonitor(RTBLogDSTE);
             //Set the name
-            this.Text = "C# Vault";
+            base.Text = @"C# Vault";
             //Hide the monitor
-            this.Height = 210;
+            Height = 210;
             //Check if any numbers in the password are more than 3 or less than 0,
             //if one is, crash the app 😏
-            foreach (int codePos in this.password) {
-                if(codePos < 1 || codePos > 3) {
-                    MessageBox.Show("Numbers in the code can't be lower then 1 or heiger then 3", "Error"
-                        ,MessageBoxButtons.OK, MessageBoxIcon.Error);
-                    //This just crashes the app
-                    throw new ArgumentOutOfRangeException("Numbers in the code can't be lower then 1 or heiger then 3");
-                }
+            if (_password.Any(codePos => codePos < 1 || codePos > 3))
+            {
+                MessageBox.Show(@"Numbers in the code can't be lower then 1 or heiger then 3", @"Error"
+                    ,MessageBoxButtons.OK, MessageBoxIcon.Error);
+                //This just crashes the app
+                throw new ArgumentOutOfRangeException("_password", 
+                    @"Numbers in the code can't be lower then 1 or heiger then 3");
             }
         }
 
-        private void cbSerialMonitorDSte_CheckedChanged(object sender, EventArgs e) {
+        private void cbSerialMonitorDSte_CheckedChanged(object sender, EventArgs e)
+        {
             //This checks if we should show the monitor
-            if (cbSerialMonitorDSte.Checked) {
-                this.Height = 450;
-            } else {
-                this.Height = 210;
-            }
+            Height = cbSerialMonitorDSte.Checked ? 450 : 210;
         }
 
         private void btnKey_Click(object sender, EventArgs e) {
             //Get the pressed button
-            Button pressedButton = (Button)sender;
+            var pressedButton = (Button)sender;
             //Convert the button text to a number
-            int pressedNumber = Convert.ToInt32(pressedButton.Text);
+            var pressedNumber = Convert.ToInt32(pressedButton.Text);
             //Add the pressed number to the history
-            enteredCode.Add(pressedNumber);
+            _enteredCode.Add(pressedNumber);
             //Write a message
-            serialMonitor.PrintLn("Input "+ enteredCode.Count + " has been set to " + pressedNumber);
-            //Set the first indicator to orange
-            if (enteredCode.Count == 1) {
-                this.pbIndicator1DSte.BackColor = Color.Orange;
-            //Set the second indicator to orange
-            } else if(enteredCode.Count == 2) {
-                this.pbIndicator2DSte.BackColor = Color.Orange;
-            //Set the third indicator to orange and perform the checks
-            } else if (enteredCode.Count == 3) {
-                this.pbIndicator3DSte.BackColor = Color.Orange;
-                //Reset the timer lifespan
-                liftime = 0;
-                //Turn all lights off
-                lightsOn = false;
-                //get the correct code
-                Boolean correctCode = enteredCode.ToArray()[0] == password[0] &&
-                    enteredCode.ToArray()[1] == password[1] &&
-                    enteredCode.ToArray()[2] == password[2];
-                //Enable the timer
-                tmrBlinkDSte.Enabled = true;
-                //Clear the history
-                enteredCode.Clear();
-                //Wat light should be pressed
-                light = correctCode ? "green" : "red";
-                //Write another message
-                serialMonitor.Print("Code correct? ");
-                if(correctCode) {
-                    //Correct code
-                    serialMonitor.PrintLn(correctCode, "g");
-                    serialMonitor.PrintLn("The vault is.... Unlocked", "g");
-                } else {
-                    //Incorrect code
-                    serialMonitor.PrintLn(correctCode, "R");
-                    serialMonitor.PrintLn("The vault is.... Locked", "r");
+            _serialMonitor.PrintLn("Input "+ _enteredCode.Count + " has been set to " + pressedNumber);
+            switch (_enteredCode.Count)
+            {
+                //Set the first indicator to orange
+                case 1:
+                    pbIndicator1DSte.BackColor = Color.Orange;
+                    //Set the second indicator to orange
+                    break;
+                case 2:
+                    pbIndicator2DSte.BackColor = Color.Orange;
+                    //Set the third indicator to orange and perform the checks
+                    break;
+                case 3:
+                {
+                    pbIndicator3DSte.BackColor = Color.Orange;
+                    //Reset the timer lifespan
+                    _lifetime = 0;
+                    //Turn all lights off
+                    _lightsOn = false;
+
+                    var codeArray = _enteredCode.ToArray();
+                    
+                    //get the correct code
+                    var correctCode = codeArray[0] == _password[0] &&
+                                          codeArray[1] == _password[1] &&
+                                          codeArray[2] == _password[2];
+                    //Enable the timer
+                    tmrBlinkDSte.Enabled = true;
+                    //Clear the history
+                    _enteredCode.Clear();
+                    //Wat light should be pressed
+                    _light = correctCode ? "green" : "red";
+                    //Write another message
+                    _serialMonitor.Print("Code correct? ");
+                    if(correctCode) {
+                        //Correct code
+                        _serialMonitor.PrintLn(true, "g");
+                        _serialMonitor.PrintLn("The vault is.... Unlocked", "g");
+                    } else {
+                        //Incorrect code
+                        _serialMonitor.PrintLn(false, "R");
+                        _serialMonitor.PrintLn("The vault is.... Locked", "r");
+                    }
+
+                    break;
                 }
             }
         }
 
         private void aboutBtnDSte_Click(object sender, EventArgs e) {
             //Set the about message
-            String m_about = "C# Vault.\n\n" +
-                "Created by:\t\tDuncan \"duncte123\" Sterken\n" +
-                "Thanks to:\t\tDuncan for coding\n" +
-                "Date:\t\t\t16 Nov 2017";
+            const string mAbout = "C# Vault.\n\n" +
+                                   "Created by:\t\tDuncan \"duncte123\" Sterken\n" +
+                                   "Thanks to:\t\tDuncan for coding\n" +
+                                   "Date:\t\t\t16 Nov 2017";
 
-            MessageBox.Show(m_about, "About");
-            serialMonitor.PrintLn(m_about, "B");
+            MessageBox.Show(mAbout, @"About");
+            _serialMonitor.PrintLn(mAbout, "B");
         }
 
         private void tmrBlink_Tick(object sender, EventArgs e) {
-            if (liftime >= 10) {
+            if (_lifetime >= 10) {
                 //Kill the timer
                 tmrBlinkDSte.Stop();
                 //Reset the pbs
-                this.pbIndicator1DSte.BackColor = DefaultBackColor;
-                this.pbIndicator2DSte.BackColor = DefaultBackColor;
-                this.pbIndicator3DSte.BackColor = DefaultBackColor;
+                pbIndicator1DSte.BackColor = DefaultBackColor;
+                pbIndicator2DSte.BackColor = DefaultBackColor;
+                pbIndicator3DSte.BackColor = DefaultBackColor;
             }
-            switch(light.ToLower()) {
+            switch(_light.ToLower()) {
                 case "green":
-                    if (lightsOn) {
-                        this.pbColorGreen.BackColor = Color.LightGreen;
-                    } else {
-                        this.pbColorGreen.BackColor = Color.Green;
-                    }
+                    pbColorGreen.BackColor = _lightsOn ? Color.LightGreen : Color.Green;
                     break;
 
                 case "red":
-                    if (lightsOn) {
-                        this.pbColorRed.BackColor = Color.Red;
-                    } else {
-                        this.pbColorRed.BackColor = Color.Maroon;
-                    }
+                    pbColorRed.BackColor = _lightsOn ? Color.Red : Color.Maroon;
                     break;
             }
-            liftime++;
+            
+            _lifetime++;
             //Toggle the light
-            lightsOn = !lightsOn;
+            _lightsOn = !_lightsOn;
         }
 
         private void btnClearDSte_Click(object sender, EventArgs e) {
             //Clear the monitor
-            serialMonitor.Clear();
+            _serialMonitor.Clear();
         }
 
         private void cbCheatDSte_CheckedChanged(object sender, EventArgs e) {
             //Show the password 😱
-            MessageBox.Show("The code is: [" + String.Join(", ", password) + "]", "Cheater!1!1!1");
-            serialMonitor.PrintLn("Code shown", "b");
+            MessageBox.Show(@"The code is: [" + String.Join(", ", _password) + ']', @"Cheater!1!1!1");
+            _serialMonitor.PrintLn("Code shown", "b");
         }
     }
 }
